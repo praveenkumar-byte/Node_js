3f0eec8a7ddb2b43b908e6e65d3e176f0a14866c
praveenkumar <praveenkumar@gmail.com>
2024-05-12 22:55:51 +0530

    last clen code

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ var bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
+app.get('/style.css', (req, res) => {
+    // Set the Content-Type header to 'text/css'
+    res.setHeader('Content-Type', 'text/css');
+
+    // Send the CSS file
+    res.sendFile(__dirname + '/style.css');
+});
+app.get('/script.js', (req, res) => {
+    try {
+        // Set the Content-Type header to 'text/javascript'
+        res.setHeader('Content-Type', 'text/javascript');
+        // Send the JavaScript file
+        res.sendFile(__dirname + '/script.js');
+    } catch (err) {
+        console.error('Error sending JavaScript file:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -37,7 +56,7 @@ app.post('/calculate', (req, res) => {
         default:
             result = 'Invalid operation';
     }
-    console.log(result)
+    // console.log(result)
     res.send({ result });
 });
 
